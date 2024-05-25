ad2d3f76116f3d1f7abccff82037f5617a5ae17f
raulgguanca <raulgguanca@gmail.com>
2024-05-24 22:21:51 -0300

    functions to create cards for movies and search filter was added

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ const apikey = "c538ffb4";
 
 let searchValue;
 let allMovies;
+let cards;
+let arrayMovies;
 
-let urlApiMovies = `http://www.omdbapi.com/?s=${searchValue}&apikey=${apikey}`;
+let urlApiMovies;
 
 async function getMovies() {
   if (searchValue == undefined) {
@@ -13,15 +15,28 @@ async function getMovies() {
       let data = await response.json();
 
       allMovies = data;
+
+      arrayMovies = allMovies.Search;
+
+      cards = addCards(arrayMovies);
+
+      paintCards();
     } catch (error) {
       console.log(error);
     }
   } else {
+    urlApiMovies = `http://www.omdbapi.com/?s=${searchValue}&apikey=${apikey}`;
     try {
       let response = await fetch(urlApiMovies);
       let data = await response.json();
 
       allMovies = data;
+
+      arrayMovies = allMovies.Search;
+
+      cards = addCards(arrayMovies);
+
+      paintCards();
     } catch (error) {
       console.log(error);
     }
@@ -53,8 +68,56 @@ function addCards(moviesCompositor) {
               <a href="./pages/details.html?id=${mov.imdbID}" class="btn">View More...</a>
             </div>
           </div>
+        </div>
     `;
     }
     noResults.innerHTML = ``;
+  } else {
+    noResults.innerHTML = `
+    <div class="no-results">
+      <h2>There's nothing to show here! Try another word or check your spelling</h2>
+      <img src="./assets/img/error.jpg" alt="error">
+    </div>
+    `;
   }
+  return moviesCards;
 }
+
+function paintCards() {
+  cardsContainer.innerHTML = cards;
+}
+
+//search filter
+const srchInput = document.getElementById("search");
+
+function checkBxFilter(list, movies) {
+  let inputFilter = [];
+
+  for (const e of movies) {
+    if (e.Title.toLowerCase().includes(list)) {
+      inputFilter.push(e);
+    }
+  }
+  return inputFilter;
+}
+
+srchInput.addEventListener("keyup", () => {
+  searchValue = srchInput.value.toLowerCase();
+  getMovies();
+  cards = addCards(checkBxFilter(searchValue, arrayMovies));
+  paintCards();
+});
+
+//input button
+const inputButton = document.getElementById("form-search-bttn");
+
+function filtrInptCrdsBttn() {
+  searchValue = srchInput.value.toLowerCase();
+  getMovies();
+  cards = addCards(checkBxFilter(searchValue, arrayMovies));
+  paintCards();
+}
+
+inputButton.addEventListener("click", () => {
+  filtrInptCrdsBttn();
+});
